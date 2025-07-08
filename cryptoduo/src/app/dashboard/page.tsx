0b0308a334e8749c92ee7cf0f3bebec93a3be57e
Aidan Wilson <aidanjwilson04@gmail.com>
2025-07-08 23:18:46 +0900

    Update page.tsx

--- cryptoduo/src/app/dashboard/page.tsx
+++ cryptoduo/src/app/dashboard/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 import { createClient } from "../../utils/supabase/server";
 import NavBar from "../../components/NavBar";
 
